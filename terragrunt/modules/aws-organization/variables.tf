0d6b6e0082b9a66bd1ccbfb46a863844264bc516
chiju <chijumelveettil@gmail.com>
2025-12-17 14:42:11 +0100

    Add missing cluster variables to aws-organization module

--- terragrunt/modules/aws-organization/variables.tf
+++ terragrunt/modules/aws-organization/variables.tf
@@ -15,3 +15,21 @@ variable "project" {
   type        = string
   default     = "tbyte"
 }
+
+variable "cluster_name" {
+  description = "EKS cluster name (not used in organization)"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_endpoint" {
+  description = "EKS cluster endpoint (not used in organization)"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_certificate_authority_data" {
+  description = "EKS cluster CA data (not used in organization)"
+  type        = string
+  default     = ""
+}
