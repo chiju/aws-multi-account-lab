f533471aa42a3fd137dae229f67a6ccaeb85fb87
chiju <chijumelveettil@gmail.com>
2025-12-17 14:40:34 +0100

    Add missing variables to aws-organization module

---
+++ terragrunt/modules/aws-organization/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "root"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "tbyte"
+}
