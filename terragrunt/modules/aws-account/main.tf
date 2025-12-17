d660704086ac81b2d2baeb721a61d251df61c5af
chiju <chijumelveettil@gmail.com>
2025-12-17 16:26:47 +0100

    Revert to safe setting: close_on_deletion=false

--- terragrunt/modules/aws-account/main.tf
+++ terragrunt/modules/aws-account/main.tf
@@ -2,7 +2,7 @@ resource "aws_organizations_account" "account" {
   name              = var.name
   email             = var.email
   parent_id         = var.parent_id
-  close_on_deletion = true
+  close_on_deletion = false
   
   tags = var.tags
   
