5b45df51996e3a7375c0ca69321ee123ecb5d44d
chiju <chijumelveettil@gmail.com>
2025-12-19 13:54:36 +0100

    Add Grafana admin password management via AWS Secrets Manager and External Secrets

--- terragrunt/modules/secrets/main.tf
+++ terragrunt/modules/secrets/main.tf
@@ -26,4 +26,29 @@ resource "aws_secretsmanager_secret_version" "x_clone_secrets" {
   }
 }
 
+# Grafana Admin Password
+resource "aws_secretsmanager_secret" "grafana_admin_password" {
+  name                    = "${var.cluster_name}-grafana-admin"
+  description             = "Grafana admin password for ${var.cluster_name}"
+  recovery_window_in_days = 0
+
+  tags = {
+    Name        = "${var.cluster_name}-grafana-admin"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Application = "grafana"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "grafana_admin_password" {
+  secret_id = aws_secretsmanager_secret.grafana_admin_password.id
+  secret_string = jsonencode({
+    admin-password = "PLACEHOLDER"
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
 # Future applications can be added here with clear comments
